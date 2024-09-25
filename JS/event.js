cf7d0a9224213cb7cfe487761cecd45294aa3db8
arojitmondal996e <arojitmondal996@gmail.com>
2024-09-25 16:25:23 +0600

    first commit

---
+++ JS/event.js
@@ -0,0 +1,25 @@
+console.log('dom event');
+// option: 1 directly set on the HTML element
+
+
+
+// option: 2
+function makeRed() {
+    document.body.style.backgroundColor = 'red';
+}
+
+// option: 3
+
+const makeBlueButton = document.getElementById('make-blue');
+// console.log(makeBlueButton);
+makeBlueButton.onclick = makeBlue;
+
+function makeBlue() {
+    document.body.style.backgroundColor = 'blue';
+}
+
+// option: 3 another
+const purpleButton = document.getElementById('make-purple');
+purpleButton.onclick = function makePurple() {
+    document.body.style.backgroundColor = 'purple'
+}
