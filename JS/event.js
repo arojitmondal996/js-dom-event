55f1e3cb84c72d550560e1844ab00734e08279e4
arojitmondal996e <arojitmondal996@gmail.com>
2024-09-28 15:13:08 +0600

    some work finished

--- JS/event.js
+++ JS/event.js
@@ -1,9 +1,10 @@
 console.log('dom event');
 // option: 1 directly set on the HTML element
+// <button onclick="console.log(65)">Another Button</button>
 
 
-
-// option: 2
+// option: 2 and onclick function on the html element
+// <button onclick="makeRed()">Make Red</button>
 function makeRed() {
     document.body.style.backgroundColor = 'red';
 }
@@ -23,3 +24,22 @@ const purpleButton = document.getElementById('make-purple');
 purpleButton.onclick = function makePurple() {
     document.body.style.backgroundColor = 'purple'
 }
+
+// option: 4
+const pinButton = document.getElementById('make-pink');
+pinButton.addEventListener('click', makePink);
+
+function makePink() {
+    document.body.style.backgroundColor = 'pink';
+}
+
+// option: 4 another
+const makeGreenButton = document.getElementById('make-green');
+makeGreenButton.addEventListener('click', function makeGreen() {
+    document.body.style.backgroundColor = 'green';
+})
+
+// option: 4 final
+document.getElementById('make-goldenrod').addEventListener('click', function makeGreen(){
+    document.body.style.backgroundColor = 'goldenrod';
+})
---
+++ JS/validate.js
@@ -0,0 +1,12 @@
+const number = '23';
+// string, boolean
+if(typeof number === 'number'){
+    console.log('value is a number')
+}
+else{
+    console.log('value is not a number')
+}
+
+// array
+const numbers = [3, 12, 78, 90];
+console.log(typeof numbers);
